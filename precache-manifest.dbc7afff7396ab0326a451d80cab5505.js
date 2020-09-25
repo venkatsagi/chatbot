3a9db6f308ff65026e04c0cba98f9ce760410aee
venkatsagi <venkatsagi7@gmail.com>
2020-09-25 04:24:39 -0400

    Updates

---
+++ precache-manifest.dbc7afff7396ab0326a451d80cab5505.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364aa70a4ed8e7bca7a0f11022f5e42d",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "748b87d5f45ecd004679",
+    "url": "/chatbot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88262754ff69aa0774cf",
+    "url": "/chatbot/static/js/2.30105061.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chatbot/static/js/2.30105061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748b87d5f45ecd004679",
+    "url": "/chatbot/static/js/main.4ae2ff9c.chunk.js"
+  },
+  {
+    "revision": "6da786bfa225ae5459b9",
+    "url": "/chatbot/static/js/runtime-main.253fb56a.js"
+  },
+  {
+    "revision": "fedc1edad7e5e6dabcaa9efac138dd5b",
+    "url": "/chatbot/static/media/tone.fedc1eda.mp3"
+  }
+]);
